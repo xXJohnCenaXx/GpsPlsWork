6bf5dd9c8d998e6d636e44cdb91d9b8c102503fa
xXJohnCenaXx <thomas@potatoplanet.org>
2015-09-22 12:57:10 -0700

    stuff
    stuff

---
+++ GPSTest/app/src/main/java/com/example/seartho16/gpstest/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.seartho16.gpstest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity
+
+
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void clickMe(View view){
+        Intent startMaps = new Intent(this,MapsActivity2.class);
+        startActivity(startMaps);    //starts the maps using the startmaps intent
+    }
+
+}
